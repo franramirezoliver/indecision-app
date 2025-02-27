a59acf9452defa91009ac259f4e452f1c3e24338
Francisco Ramírez <ramirezoliverf@gmail.com>
2025-02-27 19:04:49 +0100

    Chat con gif de YesNo

--- src/components/chat/ChatNuntii.vue
+++ src/components/chat/ChatNuntii.vue
@@ -2,6 +2,12 @@
   <div class="p-4 flex-1 overflow-y-auto">
     <div class="flex flex-col space-y-2">
       <ChatBullula :meus-est="true" nuntius="¿Salimos luego a dar una vuelta?" />
+
+      <ChatBullula
+        :meus-est="false"
+        nuntius="No"
+        imago="https://yesno.wtf/assets/yes/2-5df1b403f2654fa77559af1bf2332d7a.gif"
+      />
     </div>
   </div>
 </template>
